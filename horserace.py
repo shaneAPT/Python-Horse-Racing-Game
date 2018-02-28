2fe40eec5df456a33b0b5a104d3fe06ae5acbb01
Airidas Vekeriotas <airidas.vekeriotas2@mail.dcu.ie>
2018-02-28 21:49:39 +0000

    main menu added

---
+++ horserace.py
@@ -0,0 +1,18 @@
+current_race = "race"
+
+print "Welcome to Horse Race!" 
+print (20 * '=')
+print "Current race:", current_race
+print (20 * '=')
+print "horse, horse[odds]"
+print "horse, horse[odds]"
+print "horse, horse[odds]"
+print (20 * '=')
+print "Press:"
+print "1. To place bet"
+print "2. To start race"
+print "3. To Refresh race"
+print "4. To view horses"
+print "5. To exit" 
+
+
