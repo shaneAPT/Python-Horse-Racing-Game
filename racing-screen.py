292f6d9bacad9262378eae8332e6ba370841559b
Shane Tully <shane.tully7@mail.dcu.ie>
2018-02-28 19:50:55 +0000

    cleaned it up so theres horseX_active_pos and horseX_speed

--- racing-screen.py
+++ racing-screen.py
@@ -5,31 +5,38 @@ import random
 i = 0
 race_length = 30
 leading_horse = 0
+horse1_active_pos = 0 ## horse active positions are declared here so += is able to work
+horse2_active_pos = 0
+horse3_active_pos = 0
+horse1_speed = 1 # these speeds will be drawn from a dictionairy of the horses stats
+horse2_speed = 2
+horse3_speed = 3
+
 
 while leading_horse < race_length:
-	horse1_speed = (float(i)*.25)
-	horse2_speed = (float(i)*1.40)
-	horse3_speed = (float(i)*1.04)
+	horse1_active_pos += horse1_speed
+	horse2_active_pos += horse2_speed
+	horse3_active_pos += horse3_speed
 	
 	print " "+"="*(int(race_length)+14)+"v"+"====" ## 14 are required to make banner reach end of below strings
-	print " |||", " "*int(horse1_speed), "(horse 1>", " "*(int(race_length)-int(horse1_speed)), "|||"
-	print " |||", " "*int(horse2_speed), "(horse 2>"," "*(int(race_length)-int(horse2_speed)), "||| FINISH"
-	print " |||", " "*int(horse3_speed), "(horse 3>", " "*(int(race_length)-int(horse3_speed)), "|||"
+	print " |||", " "*int(horse1_active_pos), "(horse 1>", " "*(int(race_length)-int(horse1_active_pos)), "|||"
+	print " |||", " "*int(horse2_active_pos), "(horse 2>"," "*(int(race_length)-int(horse2_active_pos)), "||| FINISH"
+	print " |||", " "*int(horse3_active_pos), "(horse 3>", " "*(int(race_length)-int(horse3_active_pos)), "|||"
 	print " "+"="*(int(race_length)+14)+"^"+"===="
 	## print leading_horse ## purely for debug
 	time.sleep(.15)
 	i += 1
 	
-	if horse1_speed > horse2_speed and horse1_speed > horse3_speed:
-		leading_horse = horse1_speed
+	if horse1_active_pos > horse2_active_pos and horse1_active_pos > horse3_active_pos:
+		leading_horse = horse1_active_pos
 		leading_horse_name = "Horse 1"
 
-	elif horse2_speed > horse1_speed and horse2_speed > horse3_speed:
-		leading_horse = horse2_speed
+	elif horse2_active_pos > horse1_active_pos and horse2_active_pos > horse3_active_pos:
+		leading_horse = horse2_active_pos
 		leading_horse_name = "Horse 2"
 	
 	else:
-		leading_horse = horse3_speed
+		leading_horse = horse3_active_pos
 		leading_horse_name = "Horse 3"
 	
 	if leading_horse < (race_length):
