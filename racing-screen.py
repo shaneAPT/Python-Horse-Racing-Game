bc751c0a630d4549384bd4cd61c48ea968b1d1c8
Shane Tully <shane.tully7@mail.dcu.ie>
2018-02-28 02:34:11 +0000

    updated racing. not finished

--- racing-screen.py
+++ racing-screen.py
@@ -1,20 +1,38 @@
 import os
 import time
+import random
 
 i = 0
-race_length = 60
+race_length = 30
+leading_horse = 0
 
-while i < race_length:
+while leading_horse < race_length:
 	horse1_speed = (float(i)*.25)
-	horse2_speed = (float(i)*1.10)
+	horse2_speed = (float(i)*1.40)
 	horse3_speed = (float(i)*1.04)
 	
-	print " "*int(horse1_speed), "horse 1", " "*(int(race_length)-int(horse1_speed)), "|||"
-	print " "*int(horse2_speed), "horse 2", " "*(int(race_length)-int(horse2_speed)), "||| FINISH"
-	print " "*int(horse3_speed), "horse 3", " "*(int(race_length)-int(horse3_speed)), "|||"
+	print " "+"="*(int(race_length)+14)+"v"+"====" ## 14 are required to make banner reach end of below strings
+	print " |||", " "*int(horse1_speed), "(horse 1>", " "*(int(race_length)-int(horse1_speed)), "|||"
+	print " |||", " "*int(horse2_speed), "(horse 2>"," "*(int(race_length)-int(horse2_speed)), "||| FINISH"
+	print " |||", " "*int(horse3_speed), "(horse 3>", " "*(int(race_length)-int(horse3_speed)), "|||"
+	print " "+"="*(int(race_length)+14)+"^"+"===="
+	## print leading_horse ## purely for debug
 	time.sleep(.15)
-	os.system('cls' if os.name == 'nt' else 'clear')
 	i += 1
 	
-os.system('cls' if os.name == 'nt' else 'clear')
+	if horse1_speed > horse2_speed and horse1_speed > horse3_speed:
+		leading_horse = horse1_speed
+		leading_horse_name = "Horse 1"
+
+	elif horse2_speed > horse1_speed and horse2_speed > horse3_speed:
+		leading_horse = horse2_speed
+		leading_horse_name = "Horse 2"
 	
+	else:
+		leading_horse = horse3_speed
+		leading_horse_name = "Horse 3"
+	
+	if leading_horse < (race_length):
+		os.system('cls' if os.name == 'nt' else 'clear') ## if nobody has won the track refreshes and positions update
+
+print "!!! WINNER:", leading_horse_name, "!!!"
