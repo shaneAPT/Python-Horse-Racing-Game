ccbdbbb79c03a7db88f0c7697e73ebec1be4928a
Shane Tully <shane.tully7@mai.dcu.ie>
2018-04-07 00:15:57 +0100

    Finished work

--- horserace.py
+++ horserace.py
@@ -3,36 +3,34 @@ from generate_race import *
 os.system('cls' if os.name == 'nt' else 'clear') ## this clears the cmd so all you see is main menu
 
 current_race = "race"
-active_horses = generate_current_horses()
-horse_speeds = generate_horse_speed() ## speeds are generated this once and stored in a txt afterwards
+if active_horses is None:
+	active_horses = generate_current_horses()
+	horse_speeds = generate_horse_speed() ## speeds are generated this once and stored in a txt afterwards
 
 horse_stats = eval(open("horses/temp_horses.txt").read())
 horse_names = eval(open("horses/horse_names.txt").read())
-
-print horse_stats
+user_money = eval(open("user_money.txt").read())
 
 print " \t\tWelcome to Horse Race!" 
-print " "+(57 * '=')
-print " \t\tCurrent Race:", current_race
-print " ="+(55 * '_')+"= "
+print " ||"+(54 * '=')+"|| \n"
+print "\t\t", "Current Balance:", "$"+str(user_money)
+print "\tWARNING: Placing a new bet with clear \n\tany previous bet & not return your money"
+print " ||"+(54 * '_')+"|| \n"
+print "\tACTIVE HORSES:"
 print " \t\t"+active_horses[0], "<=>", horse_names[active_horses[0]]
 print " \t\t"+active_horses[1], "<=>", horse_names[active_horses[1]]
 print " \t\t"+active_horses[2], "<=>", horse_names[active_horses[2]]
 print " "+(57 * '=')
 print "\n Press:"
-print "  ]] 1. Place bet    [["
-print "  ]] 2. Start race   [["
-print "  ]] 3. Refresh race [["
-print "  ]] 4. View horses  [["
-print "  ]] 5. Exit game    [[" 
+print "  ]] 1. Start race   [["
+print "  ]] 2. Refresh race [["
+print "  ]] 3. Exit game    [[" 
 
 user_input = raw_input(" Enter a menu number:")
 
 if user_input == "1":
-	os.system("python betting_screen.py")
-
-if user_input == "2":
+	os.system('cls' if os.name == 'nt' else 'clear')
 	os.system("python racing_screen.py") ## os.system allows you to run a .py
 
-if user_input == "3":
+if user_input == "2":
 	os.system("python horserace.py")
--- racing_screen.py
+++ racing_screen.py
@@ -13,49 +13,99 @@ horse_names = []
 
 horse_stats = eval(open("horses/temp_horses.txt").read())
 horse_names_dict = eval(open("horses/horse_names.txt").read())
+user_money = eval(open("user_money.txt").read())
+
 for key, value in horse_stats.iteritems():
 	horse_names.append(key)
-
-print horse_stats
 
 for key, value in horse_stats.iteritems():
 	horse_speed_list.append(value)
 
+def print_banner():
+	print " \t\tWelcome to Horse Race!" 
+	print " ||"+(54 * '=')+"|| \n"
+	print "\t\t", "Current Balance:", "$"+str(user_money)
+	print "\tWARNING: Placing a new bet with clear \n\tany previous bet & not return your money"
+	print " ||"+(54 * '_')+"|| \n"
+	print "\tACTIVE HORSES:"
+	print " \t\t"+horse_names[0], "<=>", horse_names_dict[horse_names[0]]
+	print " \t\t"+horse_names[1], "<=>", horse_names_dict[horse_names[1]]
+	print " \t\t"+horse_names[2], "<=>", horse_names_dict[horse_names[2]]
+	print " ||"+(54 * '=')+"|| \n"
+print_banner()
+
+## ----------------- BETTING
+
+current_bet = []
+
+select_horse = input(" Select a horse lane (1, 2, 3):")
+
+while select_horse != 1 and select_horse != 2 and select_horse != 3:
+	select_horse = input(" Please select a valid horse (1-3):")
+
+current_bet.append(select_horse)
+
+select_amount = input(" How much would you like to bet?: $")
+
+while int(select_amount) > int(user_money) or select_amount == 0:
+	select_amount = input(" Select an amount greater than $0 and within your current balance of $"+str(user_money)+":")
+
+current_bet.append(select_amount)
+
+new_user_money = user_money - select_amount
+
+print " You have placed $"+str(select_amount),"on", horse_names_dict[horse_names[select_horse-1]]+"."
+print " Your current balance is now $"+str(new_user_money)+"."
+
+print "\tRace beginning..."
+time.sleep(5)
+
+## -------------- HORSES RACING
 
 while leading_horse < race_length:
-	print horse_speed_list
 	horse1_active_pos += horse_speed_list[0]
 	horse2_active_pos += horse_speed_list[1]
 	horse3_active_pos += horse_speed_list[2]
 
+	print_banner()
 	print " "+"="*(int(race_length)+13)+"v"+"====" ## 14 are required to make banner reach end of below strings
 	print " |||", " "*int(horse1_active_pos), "("+horse_names[0]+">", " "*(int(race_length)-int(horse1_active_pos)), "|||"
 	print " |||", " "*int(horse2_active_pos), "("+horse_names[1]+">", " "*(int(race_length)-int(horse2_active_pos)), "|||"
 	print " |||", " "*int(horse3_active_pos), "("+horse_names[2]+">", " "*(int(race_length)-int(horse3_active_pos)), "|||"
-	print " "+"="*(int(race_length)+13)+"^"+"===="
-	print " ||=-=\t\t", horse_names[0], "=", horse_names_dict[horse_names[0]]
-	print " ||=-=\t\t", horse_names[1], "=", horse_names_dict[horse_names[1]]
-	print " ||=-=\t\t", horse_names[2], "=", horse_names_dict[horse_names[2]]
-	print " "+"="*(int(race_length)+18)
+	print " "+"="*(int(race_length)+13)+"^"+"====\n"
+	print "\tYou currently have: $"+str(select_amount), "on lane", select_horse
+	print " ||"+(54 * '=')+"|| \n"
 	time.sleep(.15)
 	i += 1
 	
 	if horse1_active_pos > horse2_active_pos and horse1_active_pos > horse3_active_pos:
 		leading_horse = horse1_active_pos
-		leading_horse_name = "Horse 1"
+		leading_horse_name = horse_names[0]
 
 	elif horse2_active_pos > horse1_active_pos and horse2_active_pos > horse3_active_pos:
 		leading_horse = horse2_active_pos
-		leading_horse_name = "Horse 2"
+		leading_horse_name = horse_names[1]
 	
 	else:
 		leading_horse = horse3_active_pos
-		leading_horse_name = "Horse 3"
+		leading_horse_name = horse_names[2]
 	
 	if leading_horse < (race_length): ## checks if any horse position is about to exceed the track length
 		os.system('cls' if os.name == 'nt' else 'clear')
 
-print "\t\t!!! WINNER:", leading_horse_name, "!!!"
+print "\t\t!!! WINNER:", horse_names_dict[leading_horse_name], "!!!"
 
+if horse_names_dict[leading_horse_name] == horse_names_dict[horse_names[select_horse-1]]: ## if the winning name is the one selected
+	new_user_money = user_money+(select_amount*1.5)
+	print " YOUR HAVE WON: $"+ str(select_amount*1.5)+"!!\tYOUR BALANCE: $"+str(new_user_money)
+
+else:
+	new_user_money = user_money - select_amount
+
+user_money = open("user_money.txt","w")
+user_money.write(str(new_user_money))
+user_money.close()
+
+print "\tReturning to main menu..."
 time.sleep(5)
 os.system("python horserace.py") ## returns to main menu after 5 seconds
